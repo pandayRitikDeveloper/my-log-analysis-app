52f918d064bf474df02b03550cb6552ab1fb622a
Ritik Panday <panday.ritik@tftus.com>
2023-07-22 18:16:09 +0530

    task completed

---
+++ dist/index.js
@@ -0,0 +1,113 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const fs = require('fs');
+const moment = require('moment');
+const Table = require('cli-table3');
+// Function to count how many times each endpoint is called
+function countEndpointCalls(logData) {
+    const endpointCounts = {};
+    logData.forEach((logEntry) => {
+        const endpoint = logEntry.split(' ')[1];
+        endpointCounts[endpoint] = (endpointCounts[endpoint] || 0) + 1;
+    });
+    return endpointCounts;
+}
+// Function to count API calls per minute
+function countApiCallsPerMinute(logData) {
+    const apiCallsPerMinute = {};
+    logData.forEach((logEntry) => {
+        const timestamp = logEntry.split(' ')[0].slice(1, -1); // Remove square brackets from timestamp
+        const minute = moment(timestamp).format('YYYY-MM-DD HH:mm');
+        apiCallsPerMinute[minute] = (apiCallsPerMinute[minute] || 0) + 1;
+    });
+    return apiCallsPerMinute;
+}
+// Function to count total API calls for each HTTP status code
+function countApiCallsByStatusCode(logData) {
+    const apiCallsByStatusCode = {};
+    logData.forEach((logEntry) => {
+        const statusCode = logEntry.split(' ')[2];
+        apiCallsByStatusCode[statusCode] = (apiCallsByStatusCode[statusCode] || 0) + 1;
+    });
+    return apiCallsByStatusCode;
+}
+// Read the log file and parse its content
+function readLogFile(filePath) {
+    return new Promise((resolve, reject) => {
+        const logData = [];
+        fs.createReadStream(filePath)
+            .on('data', (data) => {
+            const lines = data.toString().split('\n');
+            lines.forEach((line) => {
+                if (line.trim() !== '') {
+                    logData.push(line.trim());
+                }
+            });
+        })
+            .on('end', () => resolve(logData))
+            .on('error', (error) => reject(error));
+    });
+}
+// Function to analyze a log file and display the results
+function analyzeLogFile(logFilePath) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const logData = yield readLogFile(logFilePath);
+            // Task 1: Count how many times each endpoint is called
+            const endpointCounts = countEndpointCalls(logData);
+            // Task 2: Count API calls per minute
+            const apiCallsPerMinute = countApiCallsPerMinute(logData);
+            // Task 3: Count total API calls for each HTTP status code
+            const apiCallsByStatusCode = countApiCallsByStatusCode(logData);
+            // Display data in formatted tables
+            const endpointTable = new Table({
+                head: ['Endpoint', 'Count'],
+                colWidths: [25, 10],
+            });
+            Object.entries(endpointCounts).forEach(([endpoint, count]) => {
+                endpointTable.push([endpoint, count]);
+            });
+            const minuteTable = new Table({
+                head: ['Minute', 'API Calls'],
+                colWidths: [20, 12],
+            });
+            Object.entries(apiCallsPerMinute).forEach(([minute, count]) => {
+                minuteTable.push([minute, count]);
+            });
+            const statusCodeTable = new Table({
+                head: ['Status Code', 'Count'],
+                colWidths: [25, 10],
+            });
+            Object.entries(apiCallsByStatusCode).forEach(([statusCode, count]) => {
+                statusCodeTable.push([statusCode, count]);
+            });
+            console.log(`\nEndpoint Counts for ${logFilePath}:\n`);
+            console.log(endpointTable.toString());
+            console.log(`\nAPI Calls Per Minute for ${logFilePath}:\n`);
+            console.log(minuteTable.toString());
+            console.log(`\nAPI Calls By Status Code for ${logFilePath}:\n`);
+            console.log(statusCodeTable.toString());
+        }
+        catch (error) {
+            console.error(`Error processing ${logFilePath}:`, error.message);
+        }
+    });
+}
+// Main function to run the CLI application for all log files
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const logFiles = ['api-dev-out.log', 'api-prod-out.log', 'prod-api-prod-out.log'];
+        for (const logFile of logFiles) {
+            yield analyzeLogFile(logFile);
+        }
+    });
+}
+main();
